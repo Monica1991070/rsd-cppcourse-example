91dc581bf20b3e3551edb6ab8f9b55ce67de40ad
James Hetherington <j.hetherington@ucl.ac.uk>
2013-11-01 11:00:12 +0000

    Refactor and tidy string based parsing

--- reactor/src/ReactionSystemParser.cpp
+++ reactor/src/ReactionSystemParser.cpp
@@ -3,7 +3,8 @@
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/lexical_cast.hpp>
 
-reactor::ReactionSystem * reactor::ReactionSystemParser::FromStream(std::istream & source){
+reactor::ReactionSystem * reactor::ReactionSystemParser::FromStream(std::istream & source)
+{
 	ReactionSystem *result= new ReactionSystem();
 
 	// The source stream is assumed to look like:
@@ -24,11 +25,13 @@ reactor::ReactionSystem * reactor::ReactionSystemParser::FromStream(std::istream
  		{
 	 		Reaction & reaction=result->NewReaction(rate);
  		
-		  	for (std::vector<std::string>::iterator reactant=reactant_names.begin(); reactant!=reactant_names.end(); reactant++)
+		  	for (std::vector<std::string>::iterator reactant=reactant_names.begin(); 
+		  		reactant!=reactant_names.end(); reactant++)
 			{
 				reaction.AddReactant(NewOrFind(result,*reactant));
 			}
-			for (std::vector<std::string>::iterator product=product_names.begin(); product!=product_names.end(); product++)
+			for (std::vector<std::string>::iterator product=product_names.begin(); 
+				product!=product_names.end(); product++)
 			{
 				reaction.AddProduct(NewOrFind(result,*product));
 			} 
@@ -37,7 +40,8 @@ reactor::ReactionSystem * reactor::ReactionSystemParser::FromStream(std::istream
   	return result;
 }
 
-reactor::Species * reactor::ReactionSystemParser::NewOrFind(ReactionSystem * result,const std::string &name)
+reactor::Species * reactor::ReactionSystemParser::NewOrFind(ReactionSystem * result,
+	const std::string &name)
 {
 	if (species_map.find(name) == species_map.end())
 	{
@@ -46,7 +50,21 @@ reactor::Species * reactor::ReactionSystemParser::NewOrFind(ReactionSystem * res
 	return species_map[name];
 }
 
-void reactor::ReactionSystemParser::ParseLine(std::string & line, std::vector<std::string>& reactant_names, std::vector<std::string> & product_names, double & rate)
+void reactor::ReactionSystemParser::ParseSpeciesList(std::string & source,
+	std::vector<std::string> &species_names)
+{
+	boost::split(species_names,source,boost::is_any_of("+"));
+	for (std::vector<std::string>::iterator species=species_names.begin(); 
+		species!=species_names.end(); species++)
+	{
+		boost::trim(*species);
+	}
+}
+
+void reactor::ReactionSystemParser::ParseLine(std::string & line, 
+	std::vector<std::string>& reactant_names, 
+	std::vector<std::string> & product_names, 
+	double & rate)
 {
 	std::string piece("");
 	std::string separator("");
@@ -59,16 +77,8 @@ void reactor::ReactionSystemParser::ParseLine(std::string & line, std::vector<st
 		return;
 	}
 
-	boost::split(reactant_names,sections[0],boost::is_any_of("+"));
-	boost::split(product_names,sections[2],boost::is_any_of("+"));
-	for (std::vector<std::string>::iterator reactant=reactant_names.begin(); reactant!=reactant_names.end(); reactant++)
-	{
-		boost::trim(*reactant);
-	}
-	for (std::vector<std::string>::iterator product=product_names.begin(); product!=product_names.end(); product++)
-	{
-		boost::trim(*product);
-	}
+	ParseSpeciesList(sections[0],reactant_names);
+	ParseSpeciesList(sections[2],product_names);
 	boost::trim(sections[1]);
 	rate=boost::lexical_cast<double>(sections[1]);
 
--- reactor/src/ReactionSystemParser.h
+++ reactor/src/ReactionSystemParser.h
@@ -17,11 +17,17 @@ namespace reactor {
   public:
     ReactionSystemParser(){};
     ReactionSystem * FromStream(std::istream & source);
-    void ParseLine(std::string & source, std::vector<std::string>& reactant_names, std::vector<std::string> & product_names, double & rate);
-    Species * NewOrFind(ReactionSystem * result, const std::string &name);
+    void ParseLine(std::string & source, 
+      std::vector<std::string>& reactant_names, 
+      std::vector<std::string> & product_names, 
+      double & rate);
+    Species * NewOrFind(ReactionSystem * result, 
+      const std::string &name);
     
   private:
     std::map<std::string, Species *> species_map;
+    void ParseSpeciesList(std::string & source,
+    std::vector<std::string> &species_names);
   };
 }
 
