4ef0dd89d0a4e0f3db17e330e4a52a013624df08
James Hetherington <j.hetherington@ucl.ac.uk>
2013-11-08 11:20:25 +0000

    Scaffold for final main function

--- reactor/reactor.cpp
+++ reactor/reactor.cpp
@@ -1,23 +1,18 @@
 // We write comments like this
 
 #include <iostream> //Include standard library file which provides input and output capabilities ("print statements") 
-
-#include "Species.h" // Include our own library file for describing a reaction system
 #include <boost/numeric/odeint.hpp> // Include ODE solver library, just to check our build system has picked it up ok.
 
-int main(int argument_count, char ** command_line_arguments) // Define the main function, which is the entry point to the program.
-{ // C++ uses braces to start and end sections of code.
-  
-  std::cout << "The 'boiler plate' main program is running." << std::endl; // Print out a friendly message.
-  // The << symbol is an output operator, while std::cout tells the message to go to "standard output", i.e. your terminal.
-  
-  reactor::Species calcium("Ca"); // You can declare new variables like this.
-  // "Species" is a user defined type, or "class".
-  
-  std::cout << "A species was created called \"" << calcium.GetName() << "\"" << std::endl; // You use a dot to access a method of a class.
-  // To get a quote symbol into a string, escape it with a slash
-  // << can be used to chain several outputs together, in this case, a literal string,
-  // and the output from calling GetName()
+#include "CommandLineParser.h" // Include our own library file for describing a reaction system
+#include "Solver.h"
+#include <fstream>
 
-  return 0; // Exit successfully.
+
+int main(int argument_count, const char ** command_line_arguments) // Define the main function, which is the entry point to the program.
+{ 
+    // Send the command line arguments to a command line parser
+    // Open the filename supplied as an input stream;
+    std::fstream system_file("filename_here",std::ios_base::in);
+    // Create a solver with the file stream for input and stdout for output
+    // call Solve on the solver.
 }
