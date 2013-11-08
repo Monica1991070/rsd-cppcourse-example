82ca2f627ddfe25f9da0963933284e7ad4060452
James Hetherington <j.hetherington@ucl.ac.uk>
2013-11-08 11:01:12 +0000

    Solution for solver exercise

--- reactor/src/Solver.cpp
+++ reactor/src/Solver.cpp
@@ -6,29 +6,37 @@
 reactor::Solver::Solver(std::istream & source, std::ostream & result):
 	parser(),result(result)
 {
-	// set the system using the from FromStream method of parser. 
 	system = parser.FromStream(source);
-	// Output to the result stream a header line, looking like:
-	//"#Time Ca O C\n"
+	result << "#Time " ;
+	for (std::vector<Species*>::const_iterator species_iterator=system->GetSpecies().begin();
+		species_iterator!=system->GetSpecies().end(); species_iterator++)
+	{
+				result << (*species_iterator)->GetName() << " " << std::flush;
+	}
+	result << std::endl;
 }
 
 reactor::Solver::~Solver() {
-	// Think about what to delete.
+	delete system;
 }
 
 unsigned int reactor::Solver::Solve(double start_time, double end_time, 
 		double initial_step, std::vector<double> &initial_concentrations)
 {
-	// Use SetConcentrations() on the system with the initial concentrations.
+	system->SetConcentrations(initial_concentrations);
 	return boost::numeric::odeint::integrate(boost::ref(*this), initial_concentrations, 
 		start_time, end_time, initial_step, boost::bind(&Solver::observe_integration,this,_1,_2));
 }
 
-void reactor::Solver::operator() ( const std::vector<double> &x , std::vector<double> &dxdt , const double time)
+void reactor::Solver::operator() ( const std::vector<double> &x , std::vector<double> &dxdt , const double)
 {
-	// Use GetRatesGivenConcentrations on the system to set the dxdt given the x.
+    system->GetRatesGivenConcentrations(x,dxdt);
 }
 
 void reactor::Solver::observe_integration(const std::vector<double> &concentrations , const double time){
-	// Print onto the output stream, a line consisting of the time and concentrations, separated by spaces.
+	result << time << " " << std::flush;
+	for (unsigned int i=0; i<concentrations.size();i++){
+		result << concentrations[i] <<  " " << std::flush;
+	}
+	result << std::endl;
 }
